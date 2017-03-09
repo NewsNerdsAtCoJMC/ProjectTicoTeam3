4d6912da1174e72cd0b2675890b2f404c48b6b8e
OryWickizer <ory.wickizer@gmail.com>
2017-03-09 12:13:19 -0600

    Got the app up and running on the server

--- outdoors/events/views.py
+++ outdoors/events/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Location
 
-# Create your views here.
+def index(request):
+    locations = Location.objects.all()
+    context = { 'locations': locations}
+    return render(request, 'outdoors/index.html', context)
