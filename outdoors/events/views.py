bcf6498710b277ba8366db7b31e29062015fd029
OryWickizer <ory.wickizer@gmail.com>
2017-03-14 11:11:32 -0500

    added homepage, trails map with own url, traffic map with its own url

--- outdoors/events/views.py
+++ outdoors/events/views.py
@@ -5,3 +5,13 @@ def index(request):
     locations = Location.objects.all()
     context = { 'locations': locations}
     return render(request, 'outdoors/index.html', context)
+
+def trails(request):
+    locations = Location.objects.all()
+    context = { 'locations': locations}
+    return render(request, 'outdoors/trails.html', context)
+
+def traffic(request):
+    locations = Location.objects.all()
+    context = { 'locations': locations}
+    return render(request, 'outdoors/traffic.html', context)
--- outdoors/outdoors/urls.py
+++ outdoors/outdoors/urls.py
@@ -7,4 +7,6 @@ from events import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
+    url(r'^trails/', views.trails, name='trails'),
+    url(r'^traffic/', views.traffic, name='traffic'),
 ]
